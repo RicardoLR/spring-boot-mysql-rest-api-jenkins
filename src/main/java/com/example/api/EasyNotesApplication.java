a9bd683bb0bb5811a3c15d6717f042de8a953088
Ricardo <rlopez@interware.com.mx>
2018-07-23 10:03:03 -0500

    correcion oara deploy en tomcat 8

--- src/main/java/com/example/api/EasyNotesApplication.java
+++ src/main/java/com/example/api/EasyNotesApplication.java
@@ -3,11 +3,18 @@ package com.example.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class EasyNotesApplication {
+public class EasyNotesApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EasyNotesApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EasyNotesApplication.class, args);
 	}
